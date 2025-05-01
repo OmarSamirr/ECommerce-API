fb988d6de916f9692ecb3eaadbaf1b9ad8a69f5d
Omar <omarrs200135@gmail.com>
2025-05-01 15:57:11 +0300

    Added Unit Of Work

---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _storeDbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if (_repositories.ContainsKey(typeName))
+                return (GenericRepository<TEntity, TKey>)_repositories[typeName];
+            else
+            {
+                var repo = new GenericRepository<TEntity, TKey>(_storeDbContext);
+                _repositories[typeName] = repo;
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await _storeDbContext.SaveChangesAsync();
+        }
+    }
+}
