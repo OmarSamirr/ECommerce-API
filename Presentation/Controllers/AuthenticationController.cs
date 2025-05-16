f5a8051bc650118362f50be8091b250f628ae152
Omar <omarrs200135@gmail.com>
2025-05-16 20:37:07 +0300

    Added Endpoints in AuthenticationController

--- Presentation/Controllers/AuthenticationController.cs
+++ Presentation/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServicesAbstraction;
+using Shared.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,17 @@ namespace Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(IServiceManager _serviceManager) : ControllerBase
     {
+        [HttpPost("login")]
+        public async Task<ActionResult<UserResponse>> Login(LoginRequest loginRequest)
+        {
+            return Ok(await _serviceManager.AuthenticationService.LoginAsync(loginRequest));
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<UserResponse>> Register(RegisterRequest registerRequest)
+        {
+            return Ok(await _serviceManager.AuthenticationService.RegisterAsync(registerRequest));
+        }
     }
 }
--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Domain.Contracts;
+using Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 using ServicesAbstraction;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,10 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork,
+                                IMapper _mapper,
+                                IBasketRepository _basketRepository,
+                                UserManager<ApplicationUser> _userManager) : IServiceManager
     {
         private readonly Lazy<IProductService> _lazyProductService =
             new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
@@ -18,5 +23,9 @@ namespace Services
         private readonly Lazy<IBasketService> _lazyBasketService =
             new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
         public IBasketService BasketService => _lazyBasketService.Value;
+
+        private readonly Lazy<IAuthenticationService> _lazyAuthenticationService =
+            new Lazy<IAuthenticationService>(() => new AuthenticationService(_userManager));
+        public IAuthenticationService AuthenticationService => _lazyAuthenticationService.Value;
     }
 }
